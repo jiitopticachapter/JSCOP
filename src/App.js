c339f62198759171f57ee28b4cdaf0c70df36766
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-03-20 17:03:03 +0530

    added timeline, speakers , landing page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import './App.css';
-import Footer from './Components/Footer/Footer';
-import Sponsors from './Components/Sponsors/Sponsors';
-import Team from './Components/Team/Team';
-// import Navbar from './Components/Navbar';
-// import Landing from './Components/Landing';
-// import About from './Components/About';
-// import Faq from './Components/Faq';
-// import Timeline from './Components/Timeline';
-// import Speakers from './Components/Speakers';
-// // import Gallery from './Components/Gallery';
-// import Contact from './Components/Contact';
-// import Sponsors from './Components/Sponsors';
 
-
+import "./App.css";
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import Landing from "./Components/Landing/Landing";
+import Navbar from "./Components/Navbar/Navbar";
+import Speakers from "./Components/Speaker/Speakers";
+import Sponsors from "./Components/Sponsors/Sponsors";
+import Team from "./Components/Team/Team";
+import Timeline from "./Components/Timeline/Timeline";
 
 function App() {
   return (
     <div className="app">
-      {/* <Navbar/>
-      <Landing/>
-      <About/>
-      <Speakers/>
+      {/* 
       <Faq/>
-      <Timeline/>
       <Contact/>
-     */}
+    */}
       {/* <Gallery/> */}
-      <Sponsors/>
-      <Team/> 
-      <Footer/>
 
-
+      <Navbar />
+      <Landing />
+      <About />
+      <Speakers />
+      <Timeline/>
+      <Sponsors />
+      <Team />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+import landingi from "./logo-white.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__left">
+        {/* <img src="./images/logo-black.png" alt="Optica logo" /> */}
+        <img src={landingi} alt="Optica logo" className="logo" />
+      </div>
+      <div className="navbar__right">
+        <h3> <a href="#about">About</a> </h3>
+        <h3> <a href="#timeline">Timeline</a> </h3>
+        <h3> <a href="#events">Events</a> </h3>
+        <h3> <a href="#contact-us">Contact</a> </h3>
+        <h3> <a href="#speaker">speakers</a> </h3>
+        <h3> <a href="#sponsor">sponsors</a> </h3>
+        <h3> <a href="#team">Our Team</a> </h3>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
