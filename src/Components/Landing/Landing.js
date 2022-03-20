13f16f72f2ec5528f003036bb343754e554b2b98
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-03-20 18:14:58 +0530

    updated landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -7,6 +7,18 @@ function Landing() {
     <div className='landing'>
       <ParallaxProvider>
       <Parallax speed={-50}>
+      <section className="star-animation">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </section>
        <img  className='landing__Heading' src="JSCOP@4x.png" alt="" />
        {/* <h3 className='landing__Heading'> JSCOP</h3> */}
        </Parallax>
