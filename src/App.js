e1b78456a4981e991bb1e1dc5d753e180e74d74a
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-03-26 16:19:35 +0530

    made new navbar with many changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Landing from "./Components/Landing/Landing";
 import Navbar from "./Components/Navbar/Navbar";
 import Speakers from "./Components/Speaker/Speakers";
 import Sponsors from "./Components/Sponsors/Sponsors";
-import SponsorsMain from "./Components/Sponsors/SponsorsMain";
+// import SponsorsMain from "./Components/Sponsors/SponsorsMain";
 import Team from "./Components/Team/Team";
 import Timeline from "./Components/Timeline/Timeline";
 import Sidebar from "./Components/Sidebar/Sidebar";
@@ -16,29 +16,21 @@ import Sidebar from "./Components/Sidebar/Sidebar";
 function App() {
   return (
     <div className="app">
-      <Sidebar/>
-       <Router>
-          <Navbar sticky="top"/>
-          <Switch>
-            <Route path='/' exact component={Landing} />
-            <Route path='#about' exact component={About} />
-            <Route path='#Speakers'exact component={Speakers} />
-            <Route path='/sponsor'exact component={Sponsors} />
-            <Route path='#Timeline'exact component={Timeline} />
-            <Route path='#Team'exact component={Team} />
-            <Route path='/Gallery' exact> <Gallery/> </Route>
-            <Route path='#Footer'exact component={Footer} />
-          </Switch>
-        </Router>
-      <About />
-      <Speakers />
-      <Timeline/>
-      <SponsorsMain />
-      <Team />
-      <Footer />
+      <Sidebar />
+      <Router>
+        <Navbar sticky="top" />
+        <Switch>
+          <Route path='/' exact component={Landing} />
+          <Route path='#about' exact component={About} />
+          <Route path='#Speakers' exact component={Speakers} />
+          <Route path='/sponsor' exact component={Sponsors} />
+          <Route path='#Timeline' exact component={Timeline} />
+          <Route path='#Team' exact component={Team} />
+          <Route path='/Gallery' exact> <Gallery /> </Route>
+          <Route path='#Footer' exact component={Footer} />
+        </Switch>
+      </Router>
     </div>
-    
-    
   );
 }
 
