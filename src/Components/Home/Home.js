6f5a116affef43d63569ba01a13b5fc0e5a9ee11
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-04-01 18:47:38 +0530

    made deployement changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,7 +16,7 @@ function Home() {
   return (
     <>
       <div className="home" id="home">
-      <Navbar/>
+      {/* <Navbar/> */}
       
       <section id="main" className="grid-container grid-container--home">
      
