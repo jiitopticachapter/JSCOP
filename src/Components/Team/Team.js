a0558236dd1d955af5eb62fdadb323098dabd180
Naman Gupta <75092271+namangupta06@users.noreply.github.com>
2022-04-09 21:18:12 +0530

    changed teams content

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -10,95 +10,91 @@ function Team() {
             <p className="section-subhead">
               <span>Meet &amp; Greet</span>
             </p>
-            <h2 className="section-title">the Optica Jiit team</h2>
+            <h2 className="section-title">the Organising team</h2>
           </div>
         </header>
 
         <div className="grid-content">
         <div className="page-wrapper">
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/any" alt="profile pic"/>
-      <h3>Leora D. Harrison</h3>
-      <h4>leora@ac.io</h4>
+      <img src="ayush.jfif" alt="profile pic"/>
+      <h3>Ayush Singhal</h3>
+      <h4>President</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/animal" alt="profile pic"/>
-      <h3>Eric C. Holman</h3>
-      <h4>eric@ac.io</h4>
+      <img src="Anshika.jpeg" alt="profile pic"/>
+      <h3>Anshika Jain</h3>
+      <h4>Vice President</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/animal" alt="profile pic"/>
-      <h3>Eric C. Holman</h3>
-      <h4>eric@ac.io</h4>
+      <img src="Sparsh.jpg" alt="profile pic"/>
+      <h3>Sparsh Gupta</h3>
+      <h4>Treasurer</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/animal" alt="profile pic"/>
-      <h3>Eric C. Holman</h3>
-      <h4>eric@ac.io</h4>
+      <img src="ishan.jpg" alt="profile pic"/>
+      <h3>Ishan Saxena</h3>
+      <h4>Secratary</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/animal" alt="profile pic"/>
-      <h3>Eric C. Holman</h3>
-      <h4>eric@ac.io</h4>
+      <img src="Aayush.jpg" alt="profile pic"/>
+      <h3>Aayush Mittal</h3>
+      <h4>Strategic Chief</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/animal" alt="profile pic"/>
-      <h3>Eric C. Holman</h3>
-      <h4>eric@ac.io</h4>
+      <img src="Tanmay.jpg" alt="profile pic"/>
+      <h3>Tanmay Rohilla</h3>
+      <h4>Strategic Chief</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/arch" alt="profile pic"/>
-      <h3>Christine E. Jones</h3>
-      <h4>christine@ac.io</h4>
+      <img src="Abhishek.jfif" alt="profile pic"/>
+      <h3>Abhishek Kumar Tamoli</h3>
+      <h4>Technical Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/nature" alt="profile pic"/>
-      <h3>James E. Sheldon</h3>
-      <h4>james@ac.io</h4>
+      <img src="Digvijay.jpg" alt="profile pic"/>
+      <h3>Digvijay Singh </h3>
+      <h4>Outreach Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/people" alt="profile pic"/>
-      <h3>Allan L. Mutch</h3>
-      <h4>allanLMutch@ac.io</h4>
+      <img src="harit.jpeg" alt="profile pic"/>
+      <h3>Harit Mohan</h3>
+      <h4>Design Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/any" alt="profile pic"/>
-      <h3>Kristopher N. Hastings</h3>
-      <h4>kristopher@ac.io</h4>
+      <img src="Shweta.jpeg" alt="profile pic"/>
+      <h3>Shweta Duggal</h3>
+      <h4>Operations Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/tech" alt="profile pic"/>
-      <h3>Robert S. Beers</h3>
-      <h4>robert@ac.io</h4>
+      <img src="Ananya.jpeg" alt="profile pic"/>
+      <h3>Ananya Bose</h3>
+      <h4>Content & Communication Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/any" alt="profile pic"/>
-      <h3>Brian V. England</h3>
-      <h4>brian@ac.io</h4>
+      <img src="Ritawari.jpg" alt="profile pic"/>
+      <h3>Ritawari Pareek</h3>
+      <h4>Public Relations Head</h4>
     
     </div>
     <div className="profile-box">
-      <img src="https://placeimg.com/200/200/tech" alt="profile pic"/>
-      <h3>Chung M. Starnes</h3>
-      <h4>chung@ac.io</h4>
+      <img src="Somesh.jfif" alt="profile pic"/>
+      <h3>Somesh Naruka</h3>
+      <h4>Design Head</h4>
     
     </div>
-    <div className="profile-box">
-      <img src="https://placeimg.com/200/200/any" alt="profile pic"/>
-      <h3>Mike M. Oconnell</h3>
-      <h4>mike@ac.io</h4>
-    </div>
+    
   </div>
           
           </div>
